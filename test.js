afc216c0eef3f150578116c074f815654b8cdc19
jeongdaeyun <wjdeodbs123@gmail.com>
2019-06-09 19:24:39 +0900

    update

---
+++ test.js
@@ -0,0 +1,29 @@
+
+
+// var sayNode = function(){
+//     console.log('Node');
+// };
+
+// var es = "ES";
+// var oldObject = {
+//     sayJS: function(){
+//         console.log('JS');
+//     },
+//     sayNode: sayNode,
+// };
+
+// oldObject[es + 6] = 'Fantastic';
+// oldObject.sayNode();
+// oldObject.sayJS();
+// console.log(oldObject.ES6);
+
+// const newObject = {
+//     sayJS(){
+//         console.log('JS');
+//     },
+//     sayNode,
+//     [es+6]: 'Fantastic',
+// }; // 기존의 sayJS : function => sayJS(){}. sayNode:sayNode => sayNode 
+// newObject.sayNode();
+// newObject.sayJS();
+// console.log(newObject.ES6);
